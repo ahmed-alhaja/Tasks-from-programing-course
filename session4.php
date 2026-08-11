b07e501c8a18b0ee017bb164d12fc111564a6c0d
ahmed-alhaja GitHub <ahmdalhajh79@gmail.com GitHub>
2026-08-11 16:30:21 +0300

    Update session4

--- session4.php
+++ session4.php
@@ -23,25 +23,110 @@
     $numbers = [1, 1, 2, 1, 2, 3, 6, 7, 7, 4, 5, 5]; //  number = 2
     $filterSortNumbers = []; // 1  , 1 , 2 , 1 
 
-    foreach ($numbers as $number) { // 2
+    $i = 0;
+
+    while ($i < count($numbers)) {
         $found = false;
-        foreach ($filterSortNumbers as $filterSortNumber) { // 
-            if ($filterSortNumber = $number) { // number = 2 || 1 == 2 ? 1 == 2? 2==2 ? found = true || break 
+
+        $j = 0;
+
+        while ($j < count($filterSortNumbers)) {
+            if ($filterSortNumbers[$j] == $numbers[$i]) {
                 $found = true;
                 break;
             }
+
+            $j++;
         }
+
         if ($found == false) {
-            $filterSortNumbers[] = $number;
+            $filterSortNumbers[] = $numbers[$i];
         }
+
+        $i++;
     }
+    // foreach ($numbers as $number) { // 2
+    //     $found = false;
+    //     foreach ($filterSortNumbers as $filterSortNumber) { // 
+    //         if ($filterSortNumber == $number) { // number = 2 || 1 == 2 ? 1 == 2? 2==2 ? found = true || break 
+    //             $found = true;
+    //             break;
+    //         }
+    //     }
+    //     if ($found == false) {
+    //         $filterSortNumbers[] = $number;
+    //     }
+    // }
     sort($filterSortNumbers);
+    // print_r($filterSortNumbers);
 
     // task 3 
-    for ($i=0; $i < count($numbers) ; $i++) { 
-        echo $i . "<br>";
+    $text = 'erasoftrr';
+    $count = 0;
+    $i = 0;
+    while ($i < strlen($text)) {
+        if ($text[$i] == 'r') {
+            $count++;
+        }
+        $i++;
+    }
+    $count . "<br>";
+    // task 4
+    $i = 0;
+    while ($i <= 50) {
+
+        if (($i % 3) == 0 && ($i % 5) == 0) {
+            'FizzBuzz' . '<br>';
+        } else if (($i % 5) == 0) {
+            'Buzz' . '<br>';
+        } else if (($i % 3) == 0) {
+            'Fizz' . '<br>';
+        } else {
+            $i . "<br>";
+        }
+        $i++;
+    }
+    // task 5
+    $i = 0;
+    $sum = 0;
+    for ($i = 0; $i <= 30; $i++) {
+        $sum += $i;
+    }
+    echo $sum . "<br>";
+
+    // task 6
+    $fun_Numbers = [5, 15, -10, 100, 250, 0, 1];
+    $largestNum = 0;
+    $smallestNum = 0;
+    function searchLargeNumber(array $nums, int $largestNum)
+    {
+        foreach ($nums as $num) {
+            if ($num > $largestNum) {
+                $largestNum = $num;
+            }
+        }
+        return $largestNum;
     }
 
+    echo searchLargeNumber($fun_Numbers, $largestNum) . "<br>";
+
+    //  task 7 
+    function searchsmallNumber(array $nums, int $largestNum)
+    {
+        foreach ($nums as $num) {
+            if ($num < $largestNum) {
+                $largestNum = $num;
+            }
+        }
+        return $largestNum;
+    }
+    echo searchsmallNumber($fun_Numbers, $largestNum) . "<br>";
+    // task 8
+    $factorial = 1;
+    for ($i = 1; $i <= 5; $i++) {
+        $factorial *= $i;
+    }
+    echo $factorial;
     ?>
 </body>
 
