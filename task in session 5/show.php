77bb7be4727c306629abc9c840c642d304eeceb9
ahmed-alhaja GitHub <ahmdalhajh79@gmail.com GitHub>
2026-08-16 01:13:20 +0300

    Add programming course tasks

---
+++ task in session 5/show.php
@@ -0,0 +1,7 @@
+<?php
+
+
+$showName = $_POST['name'] ?? null;
+$showEmail = $_POST['email'] ?? null;
+$showPhone = $_POST['phone'] ?? null;
+echo "My name is: $showName and My email is: $showEmail and My phone number is: $showPhone";
