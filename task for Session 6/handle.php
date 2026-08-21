1913b26a0fa0b324f5a4229f21ca83da0370c340
ahmed-alhaja GitHub <ahmdalhajh79@gmail.com GitHub>
2026-08-21 18:25:44 +0300

    Add Session 6

---
+++ task for Session 6/handle.php
@@ -0,0 +1,7 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$phone = $_POST['phone'];
+echo "My name is $name and my email is $email and my phone is $phone";
